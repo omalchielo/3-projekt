de2e5c854854d16e7fd56284fdd6e2ca7bd060bc
omalchielo <74373870+omalchielo@users.noreply.github.com>
2021-09-18 14:55:49 +0200

    Update election-scraper.py

--- election-scraper.py
+++ election-scraper.py
@@ -1,38 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 import xlsxwriter
+import sys
 
-def main(URL):
+
+def main(URL, misto):
+
+    print("Beru data z", URL)
+    print("Zapisuji data do", misto)
     r = requests.get(URL)
     soup = BeautifulSoup(r.content, "html.parser", from_encoding="utf8")
-    workbook = xlsxwriter.Workbook('projekt_3.xlsx')
+    workbook = xlsxwriter.Workbook(f'{misto}.xlsx')
     worksheet = workbook.add_worksheet()
-    m = q = s = 1
-    voh(soup,worksheet)
-    zapis_cisla(soup,worksheet,q,m)
-    volebni_strany(worksheet,workbook)
-def voh(soup,worksheet):
+
+    voh(soup, worksheet)
+    zapis_cisla(soup, worksheet)
+    volebni_strany(worksheet, workbook)
+
+
+def voh(soup, worksheet):
     m = 1
     n = 1
     celkem_list = []
 
     for tag in soup.find_all("a"):
+
         kod = str(tag.string)
         if kod.isdigit():
-            m += 1
 
+            m += 1
             URL1 = f"https://volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=12&xobec={kod}&xvyber=7103"
             r = requests.get(URL1)
             soup1 = BeautifulSoup(r.content, "html.parser", from_encoding="utf8")
-            for neco in soup1.find_all("td",class_="cislo",headers="t1sa2 t1sb3"):
+            for neco in soup1.find_all("td", class_="cislo", headers="t1sa2 t1sb3"):
+
                 celkem = neco.get_text()
                 celkem_list.append(celkem)
-                
-            for neco in soup1.find_all("td",class_="cislo",headers="t2sa2 t2sb3"):
+            for neco in soup1.find_all("td", class_="cislo", headers="t2sa2 t2sb3"):
+
                 celkem1 = neco.get_text()
                 celkem_list.append(celkem1)
 
-            worksheet.write_row(n,5,celkem_list)
+            worksheet.write_row(n, 5, celkem_list)
             celkem_list = []
             n += 1
             volici = soup1.find("td", class_="cislo", headers="sa2")
@@ -48,11 +57,13 @@ def voh(soup,worksheet):
             worksheet.write(f"E{m}", hlasy_ok)
             zapis_nazvy(worksheet)
 
-def zapis_nazvy(worksheet):
-    hlavicka = ["KOD","JMENO","VOLICI","OBALKY","HLASY"]
-    worksheet.write_row(0,0,hlavicka)
 
-def zapis_cisla(soup,worksheet,q,m):
+def zapis_nazvy(worksheet):
+    hlavicka = ["KOD", "JMENO", "VOLICI", "OBALKY", "HLASY"]
+    worksheet.write_row(0, 0, hlavicka)
+
+
+def zapis_cisla(soup, worksheet):
     q = 1
     for tag in soup.find_all("td", headers="t1sa1 t1sb2"):
         jmeno = tag.get_text()
@@ -69,7 +80,9 @@ def zapis_cisla(soup,worksheet,q,m):
         q += 1
         if jmeno2 != "-":
             worksheet.write(f"B{q}", jmeno2)
-def volebni_strany(worksheet,workbook):
+
+
+def volebni_strany(worksheet, workbook):
     URL2 = "https://volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=12&xobec=506761&xvyber=7103"
     r = requests.get(URL2)
     soup = BeautifulSoup(r.content, "html.parser", from_encoding="utf8")
@@ -82,10 +95,16 @@ def volebni_strany(worksheet,workbook):
     for tag in soup.find_all("td", headers="t2sa1 t2sb2"):
         strany1 = tag.get_text()
         list_stran.append(strany1)
-
+    print("Ukončuji")
     worksheet.write_row(0, 5, list_stran)
 
     workbook.close()
 
 
-main("https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7101")
+if __name__ == "__main__":
+    URL = sys.argv[1]
+    misto = sys.argv[2]
+    if "https" not in URL:
+        print("Zadej spravnou adresu!")
+        quit()
+    main(URL, misto)
